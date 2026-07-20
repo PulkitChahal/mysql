b42de5f50214c457af45b1d599da832f72cae62e
Pulkit Chahal <108520687+PulkitChahal@users.noreply.github.com>
2026-07-20 18:31:08 +0530

    Time: 577 ms (15.62%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,14 @@
-SELECT w1.id
-FROM Weather w1
-JOIN Weather w2
-    ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
-WHERE w1.temperature > w2.temperature;
+-- SELECT w1.id
+-- FROM Weather w1
+-- JOIN Weather w2
+--     ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+-- WHERE w1.temperature > w2.temperature;
+
+SELECT current_day.id
+FROM Weather AS current_day
+WHERE EXISTS (
+    SELECT 1
+    FROM Weather AS yesterday
+    WHERE current_day.temperature > yesterday.temperature
+    AND current_day.recordDate = yesterday.recordDate + INTERVAL 1 DAY
+);
