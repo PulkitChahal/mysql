56ef015b8dd2b2113a0a5f09e5db25fae73c9fa1
Pulkit Chahal <108520687+PulkitChahal@users.noreply.github.com>
2026-07-22 22:40:34 +0530

    Time: 1831 ms (10.43%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,11 +1,20 @@
-WITH cte AS(
-    SELECT sales_id
-    FROM Orders o 
-    LEFT JOIN Company c
-    ON o.com_id = c.com_id
-    WHERE c.name LIKE 'RED'
-)
+-- WITH cte AS(
+--     SELECT sales_id
+--     FROM Orders o 
+--     LEFT JOIN Company c
+--     ON o.com_id = c.com_id
+--     WHERE c.name LIKE 'RED'
+-- )
 
-SELECT name
-FROM SalesPerson
-WHERE sales_id NOT IN (SELECT DISTINCT(sales_id) FROM cte);
+-- SELECT name
+-- FROM SalesPerson
+-- WHERE sales_id NOT IN (SELECT DISTINCT(sales_id) FROM cte);
+
+SELECT s.name
+FROM Orders o
+JOIN Company c
+    ON o.com_id = c.com_id
+   AND c.name = 'RED'
+RIGHT JOIN SalesPerson s
+    ON s.sales_id = o.sales_id
+WHERE o.sales_id IS NULL;
