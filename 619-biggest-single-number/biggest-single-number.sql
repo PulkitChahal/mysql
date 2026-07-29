e879f54f82d2007bdf9bd119960183b3741db291
Pulkit Chahal <108520687+PulkitChahal@users.noreply.github.com>
2026-07-29 14:49:56 +0530

    Time: 483 ms (26.53%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,7 +1,14 @@
-SELECT MAX(num) AS num
-FROM (
-    SELECT num
-    FROM MyNumbers
-    GROUP BY num
-    HAVING COUNT(*) = 1
-) AS t;
+-- SELECT MAX(num) AS num
+-- FROM (
+--     SELECT num
+--     FROM MyNumbers
+--     GROUP BY num
+--     HAVING COUNT(*) = 1
+-- ) AS t;
+
+SELECT 
+    CASE WHEN COUNT(num) = 1 THEN num ELSE NULL END AS num
+FROM MyNumbers
+GROUP BY num
+ORDER BY num DESC
+LIMIT 1;
