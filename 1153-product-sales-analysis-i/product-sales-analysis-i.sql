0c3a7f84f0134f31e27b1d5ffdfdcf0dea48fac8
Pulkit Chahal <108520687+PulkitChahal@users.noreply.github.com>
2026-07-29 18:40:48 +0530

    Time: 1524 ms (15.97%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,22 +1,22 @@
--- SELECT 
---     p.product_name,
---     s.year,
---     s.price
--- FROM Sales s
--- LEFT JOIN Product p
--- ON s.product_id = p.product_id;
-
-
 SELECT 
-    sub.product_name,
+    p.product_name,
     s.year,
     s.price
-FROM 
-    Sales s,
-    (SELECT 
-         p.product_id, 
-         p.product_name 
-     FROM 
-         Product p) sub
-WHERE 
-    s.product_id = sub.product_id;
+FROM Sales s
+LEFT JOIN Product p
+ON s.product_id = p.product_id;
+
+
+-- SELECT 
+--     sub.product_name,
+--     s.year,
+--     s.price
+-- FROM 
+--     Sales s,
+--     (SELECT 
+--          p.product_id, 
+--          p.product_name 
+--      FROM 
+--          Product p) sub
+-- WHERE 
+--     s.product_id = sub.product_id;
