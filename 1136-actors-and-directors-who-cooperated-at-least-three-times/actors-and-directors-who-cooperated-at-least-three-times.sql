11c7339052bc53f1b51ee43092e6df4b56552efd
Pulkit Chahal <108520687+PulkitChahal@users.noreply.github.com>
2026-07-29 18:26:10 +0530

    Time: 413 ms (25.94%) | Memory: 0B (100.00%) - LeetSync

--- 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,6 +1,15 @@
-SELECT 
-    actor_id,
-    director_id
-FROM ActorDirector
-GROUP BY actor_id, director_id
-HAVING COUNT(*) >= 3;
+-- SELECT 
+--     actor_id,
+--     director_id
+-- FROM ActorDirector
+-- GROUP BY actor_id, director_id
+-- HAVING COUNT(*) >= 3;
+
+SELECT actor_id, director_id 
+FROM(
+        SELECT actor_id, director_id, 
+        COUNT(timestamp) as cooperated 
+        FROM ActorDirector 
+        GROUP BY actor_id, director_id
+    ) table1
+WHERE cooperated >= 3;
