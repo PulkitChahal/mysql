e7d14ee4d042df1ab678e86729ed754baaee73bd
Pulkit Chahal <108520687+PulkitChahal@users.noreply.github.com>
2026-07-30 15:13:44 +0530

    Time: 1615 ms (7.55%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -1,3 +1,16 @@
+-- SELECT
+--     p.product_id,
+--     p.product_name
+-- FROM Product p
+-- JOIN Sales s
+--     ON p.product_id = s.product_id
+-- GROUP BY
+--     p.product_id,
+--     p.product_name
+-- HAVING
+--     MIN(s.sale_date) >= '2019-01-01'
+--     AND MAX(s.sale_date) <= '2019-03-31';
+
 SELECT
     p.product_id,
     p.product_name
@@ -8,5 +21,10 @@ GROUP BY
     p.product_id,
     p.product_name
 HAVING
-    MIN(s.sale_date) >= '2019-01-01'
-    AND MAX(s.sale_date) <= '2019-03-31';
+    SUM(
+        CASE
+            WHEN s.sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31'
+            THEN 1
+            ELSE 0
+        END
+    ) = 0;
